6b210b391c4dbd7f3000431882e5faf3094674c7
Jakob Steege <j.steege@dar-gmbh.com>
2024-03-05 09:50:55 +0100

    authentification, blogpost published at

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,7 @@
 class BlogPost < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true
+    scope :draft, -> {where( published_at: nil) }
+    scope :published, -> {where( "published_at <= ?": Time.current) }
+    scope :scheduled, -> {where( "published_at > ?": Time.current) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_05_073654) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_05_084144) do
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
