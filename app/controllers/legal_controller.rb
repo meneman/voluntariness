b38e0fd712fbba92d16c2e8fc19cd22f3a3b7a02
meneman <ja.steege@gmail.com>
2025-08-02 11:15:48 +0200

    routing fixed

--- app/controllers/legal_controller.rb
+++ app/controllers/legal_controller.rb
@@ -1,4 +1,7 @@
 class LegalController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:terms, :privacy]
+  skip_before_action :ensure_current_household, only: [:terms, :privacy]
+
   def terms
   end
 
